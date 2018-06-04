336b1d23f5582d5f64a79be470924c755ed2fca5
Victor Chong <victor.chong@linaro.org>
2018-06-04 16:17:13 +0900

    use proper hal module naming

--- keymaster/v2.0/Android.mk
+++ keymaster/v2.0/Android.mk
@@ -35,7 +35,7 @@ include $(CLEAR_VARS)
 ifeq ($(USE_32_BIT_KEYSTORE), true)
 LOCAL_MULTILIB := 32
 endif
-LOCAL_MODULE := keystore2_0.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := keystore.$(TARGET_BOARD_PLATFORM)
 
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 21 && echo OK),OK)
 	LOCAL_MODULE_RELATIVE_PATH := hw
